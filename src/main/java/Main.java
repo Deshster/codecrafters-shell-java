7e894b6e7d15265d5c29e81f94aa4ff34d8537e8
eves <eviepeezy@duck.com>
2026-06-12 19:29:48 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
+    static String path = System.getenv("PATH");        
+    static String[] pathDirs = path.split(":");
+    
     public static void main(String[] args) throws Exception {       
         Scanner scanner = new Scanner(System.in);
-        
+
         while(true) {
             System.out.print("$ ");
 
@@ -13,16 +17,32 @@ public class Main {
                 break;
             } else if (command.startsWith("echo ")) {
                 System.out.println(command.substring(5));
-            } else if (command.startsWith("type ")) {
-                if (command.substring(5).equals("exit") || command.substring(5).equals("echo") || command.substring(5).equals("type")) { 
-                    System.out.println(command.substring(5) + " is a shell builtin");
-                } else {
-                    System.out.println(command.substring(5) + " not found"); 
-                }
+            } else if (command.startsWith("type ")) { 
+                System.out.println(type(command.substring(5)));
             }           
             else {
                 System.out.println(command + ": command not found");
             }
         }
+    }
+
+    public static String type(String command) {
+        String[] builtins = {"exit", "echo", "type"};
+        
+        boolean isBuiltin = false;
+        for (int i = 0; i < builtins.length; i++) {
+            if (builtins[i].equals(command)) {
+                return command + " is a shell builtin";
+            }
+        }
+
+        for (int i = 0; i < pathDirs.length; i++) {
+            File file = new File(pathDirs[i], command);
+            if (file.exists() && file.canExecute()) {
+                return command + " is " + file.getAbsolutePath();
+            }
+        }
+
+        return command + ": not found";
     }
 }
