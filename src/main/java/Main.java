a2454a66585b99722fbefcd1498e6b92f4511956
eves <eviepeezy@duck.com>
2026-06-12 18:07:46 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,9 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             String command = scanner.nextLine();
 
+            if (command.equals("exit")) {
+                break;
+            }
             System.out.println(command + ": command not found");
         }
     }
