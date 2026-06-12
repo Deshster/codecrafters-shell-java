e9c36e9c92e4852b12faa36ee7889a8d3f9f62e2
eves <eviepeezy@duck.com>
2026-06-12 18:09:32 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,10 @@ public class Main {
 
             if (command.equals("exit")) {
                 break;
+            } else if (command.startsWith("echo ")) {
+                System.out.println(command.substring(5));
             }
+
             System.out.println(command + ": command not found");
         }
     }
